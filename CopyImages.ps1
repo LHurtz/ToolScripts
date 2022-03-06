3c8647547b7815d546932e92ab761b8f827a423d
Lukas Hurtz <lukas@am-nordpark.de>
2022-03-06 13:24:51 +0100

    Refactored to function

--- CopyImages.ps1
+++ CopyImages.ps1
@@ -5,17 +5,33 @@ param(
     [String] $destination = '.\Dest'
 )
 
-Get-ChildItem -Path $sourcePath -Include *.jpg, *.png -File -Recurse | ForEach-Object {
-    $extensionNoPoint = $_.Extension -replace '[.]'
-    $extensionDirectory = Join-Path $destination $extensionNoPoint.ToLower()
-    $filePath = Join-Path -Path $extensionDirectory -ChildPath $_.Name
+function CopyFile
+{
+    param
+    (
+        [parameter(Mandatory=$true)]
+        [System.IO.FileInfo] $file
+    )
+    process
+    {
+        $extensionNoPoint = $file.Extension -replace '[.]'
+        $extensionDirectory = Join-Path $destination $extensionNoPoint.ToLower()
+        $filePath = Join-Path -Path $extensionDirectory -ChildPath $file.Name
 
+        # if the destination folder does not already exist, create it
+        if (!(Test-Path -Path $extensionDirectory -PathType Container)) {
+            $null = New-Item -Path $extensionDirectory -ItemType Directory
+        }
 
-    # if the destination folder does not already exist, create it
-    if (!(Test-Path -Path $extensionDirectory -PathType Container)) {
-        $null = New-Item -Path $extensionDirectory -ItemType Directory
+        $file | Copy-Item -Destination $filePath
     }
+}
 
+$files = Get-ChildItem -Path $sourcePath -Include *.jpg, *.png -File -Recurse
+$fileCount = $files.Count
 
-    $_ | Copy-Item -Destination $filePath
+Write-Host "$fileCount Files to copy"
+
+foreach ($file in $files) {
+    CopyFile -file $file
 }
